6c8b9f08f2cc2b0a5dca15d8c52b086ff35ce7b2
tim <timmywilliams4665@gmail.com>
2025-01-21 12:59:42 -0800

    added logic for space in a string still need to work on periods commas etc

--- src/LowercaseSentenceTokenizer.java
+++ src/LowercaseSentenceTokenizer.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -30,7 +31,14 @@ public class LowercaseSentenceTokenizer implements Tokenizer {
    */
   public List<String> tokenize(Scanner scanner) {
     // TODO: Implement this function to convert the scanner's input to a list of words and periods
-    return null;
+    
+    String input = scanner.nextLine();
+
+    String[] convertToArray = input.split(" ");
+    
+    return Arrays.asList(convertToArray);
+
+
   }
 }
 
