ba2e5aa0b5cb95c9874b468d3a6f7c3bc97bad43
JSaiyan <183049245+JSaiyan@users.noreply.github.com>
2025-01-26 18:13:53 -0800

    created testtokenizewithspaces

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -3,7 +3,8 @@ import java.util.List;
 import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.*;
 
-class LowercaseSentenceTokenizerTest {
+class LowercaseSentenceTokenizerTest 
+{
 
     // Wave 1
     @Test
@@ -16,9 +17,17 @@ class LowercaseSentenceTokenizerTest {
     }
 
     // Wave 2
-    /*
-     * Write your test here!
-     */
+    @Test
+    void testTokenizeWithSpaces()
+    {
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("hello     hi hi hi    hello hello");
+        List<String> tokens = tokenizer.tokenize(scanner);
+
+        assertEquals(List.of("hello", "hi", "hi", "hi", "hello", "hello"), tokens);
+
+    }
+
     
 
     // Wave 3
