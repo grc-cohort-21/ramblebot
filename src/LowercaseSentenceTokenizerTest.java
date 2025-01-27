f1b65a84b8a0323a5b70e99bc92d3f3e10209736
Nur Mohamed <mohamed.nur@student.greenriver.edu>
2025-01-27 15:48:18 -0800

    wave1, wav2 is completed

--- src/LowercaseSentenceTokenizerTest.java
+++ src/LowercaseSentenceTokenizerTest.java
@@ -19,7 +19,15 @@ class LowercaseSentenceTokenizerTest {
     /*
      * Write your test here!
      */
-    
+    @Test
+    void testNoSpaceToken() {
+        LowercaseSentenceTokenizer tokenizer = new LowercaseSentenceTokenizer();
+        Scanner scanner = new Scanner("hello       hi hi hi          hello hello");
+        List<String> tokens = tokenizer.tokenize(scanner);
+
+        assertEquals(List.of("hello", "hi", "hi", "hi", "hello", "hello"), tokens);
+    }
+
 
     // Wave 3
     @Test
