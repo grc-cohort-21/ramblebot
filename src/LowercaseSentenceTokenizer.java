483ca0281f3e558a24d231a4b829f5adf76ebb78
JACOB_WOODBURY <166659344+JacobWoodbury@users.noreply.github.com>
2025-01-27 10:49:29 -0800

    wave 1 tokenizer

--- src/LowercaseSentenceTokenizer.java
+++ src/LowercaseSentenceTokenizer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -30,7 +31,12 @@ public class LowercaseSentenceTokenizer implements Tokenizer {
    */
   public List<String> tokenize(Scanner scanner) {
     // TODO: Implement this function to convert the scanner's input to a list of words and periods
-    return null;
+    ArrayList<String> tokens = new ArrayList<>();
+    while (scanner.hasNext()) {
+      tokens.add(scanner.next().toLowerCase());
+      tokens.add(scanner.next().toLowerCase());
+    }
+    return tokens;
   }
 }
 
