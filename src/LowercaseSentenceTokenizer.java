f5844b6a42078770a553ce520b530094187ee039
Hunter Y <huntersy99@gmail.com>
2025-01-27 17:54:02 -0800

    First attempt on implementing LowercaseSentenceTokenizer, probably won't work but fingers crossed

--- src/LowercaseSentenceTokenizer.java
+++ src/LowercaseSentenceTokenizer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -30,7 +31,19 @@ public class LowercaseSentenceTokenizer implements Tokenizer {
    */
   public List<String> tokenize(Scanner scanner) {
     // TODO: Implement this function to convert the scanner's input to a list of words and periods
-    return null;
+    List<String> tokenList = new ArrayList<>();
+    while (scanner.hasNext()) {
+      String token = scanner.next().toLowerCase();
+      if (token.endsWith(".")) {
+        token = token.substring(0, -1);
+        tokenList.add(token);
+        tokenList.add(".");
+      }
+      else {
+        tokenList.add(token);
+      }
+    }
+
+    return tokenList;
   }
 }
-
